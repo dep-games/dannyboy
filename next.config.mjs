a42f55bc016c07684f82e3aa0e536d360150d1f3
dep-games <danny.boy.developer@gmail.com>
2024-07-21 23:36:26 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,7 @@ import withNextIntl from "next-intl/plugin";
 // );
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         hostname: "skillicons.dev",
